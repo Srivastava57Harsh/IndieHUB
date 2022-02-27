3ac1a893fba43a0952d14d8260f730e7919c10f7
Srivastava57harsh <srivastava57harsh@gmail.com>
2022-02-27 11:49:04 +0530

    :bug: minor changes
    minor changes

--- src/components/TopArtists.jsx
+++ src/components/TopArtists.jsx
@@ -40,7 +40,7 @@ export default function Faq() {
             class="relative items-center justify-start inline-block px-[2vw] py-[2vh] overflow-hidden font-bold rounded-md group"
           >
             <span class="w-32 h-32 rotate-45 translate-x-12 -translate-y-2 absolute left-0 top-0 bg-white opacity-[3%]"></span>
-            <span class="absolute top-0  left-0 w-52 h-48 -mt-1 transition-all duration-500 ease-in-out rotate-[35deg] -translate-x-56 -translate-y-24 bg-[#FC4754] opacity-100 group-hover:-translate-x-8"></span>
+            <span class="absolute top-0  left-0 w-[28.5vh] h-48 -mt-1 transition-all duration-500 ease-in-out rotate-[30deg] -translate-x-56 -translate-y-24 bg-[#FC4754] opacity-100 group-hover:-translate-x-8"></span>
             <span class="relative w-full text-left text-white transition-colors duration-200 ease-in-out group-hover:text-gray-900">
               Support them
             </span>
